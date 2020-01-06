bebe039716183e0e20f4b1a38774cb667c7194fb
Kilseic <leoncieslikdesign@gmail.com>
2020-01-06 17:06:40 +0000

    Added operators using switch

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const response = readline.prompt();
 console.log('Please enter your second number:');
 const response_2 = readline.prompt();
 var out
-if (op == "+") {
-    out = +response + +response_2;
-} else if (op == "-") {
-    out = +response - +response_2;
-} else if (op == "/") {
-    out = +response / +response_2;
-} else if (op == "*") {
-    out = +response * +response_2;
-} else {
-    throw new UserException("Operator not valid.")
+switch (op) {
+    case '+':
+        out = +response + +response_2;
+        break;
+    case '-':
+        out = +response - +response_2;
+        break;
+    case '/':
+        out = +response / +response_2;
+        break;
+    case '*':
+        out = +response * +response_2;
+        break;
+    default:
+        throw ('Not a valid operator.')
 }
-console.log("The answer is:" + out);
+console.log('The answer is: ' + out);
