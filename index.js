ef9c54014a0e1a2793bd484b1f3047f426a87f5d
Kilseic <leoncieslikdesign@gmail.com>
2020-01-06 16:59:27 +0000

    Added operators using if

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const readline = require('readline-sync');
-//console.log('Welcome to the calculator, yay!');
+console.log('Welcome to the calculator!');
+console.log('==========================');
+console.log('Please enter the operator:');
+const op = readline.prompt();
 console.log('Please enter your first number:');
 const response = readline.prompt();
 console.log('Please enter your second number:');
 const response_2 = readline.prompt();
-console.log(+response * +response_2);
+var out
+if (op == "+") {
+    out = +response + +response_2;
+} else if (op == "-") {
+    out = +response - +response_2;
+} else if (op == "/") {
+    out = +response / +response_2;
+} else if (op == "*") {
+    out = +response * +response_2;
+} else {
+    throw new UserException("Operator not valid.")
+}
+console.log("The answer is:" + out);
