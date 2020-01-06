d68f91e0eb4895e713517fde49efa42a42e48538
Kilseic <leoncieslikdesign@gmail.com>
2020-01-06 16:38:57 +0000

    Added a simple multiplier

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ console.log('Please enter your first number:');
 const response = readline.prompt();
 console.log('Please enter your second number:');
 const response_2 = readline.prompt();
-const out = +response * +response_2;
-console.log(out);
+console.log(+response * +response_2);
