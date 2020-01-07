fb9e5ee5f7f5fd2c8118cf17cddce9718fbd40ab
Kilseic <leoncieslikdesign@gmail.com>
2020-01-07 16:06:22 +0000

    Refactored using functions and continuously runs

--- index.js
+++ index.js
@@ -1,34 +1,60 @@
 const readline = require('readline-sync');
-console.log('Welcome to the calculator!');
-console.log('==========================');
-console.log('Please enter the operator:');
-const op = readline.prompt();
-console.log('How many numbers would you like to ' + op + '?')
-const n = readline.prompt();
-let arr = [];
-for (let i = 0; i < n; i++) {
-    console.log('Please enter number ' + (i+1) +': ');
-    arr[i] = +readline.prompt();
+printWelcomeMessage();
+while (true) {
+    performOneCalculation();
 }
-var out = arr[0];
-switch (op) {
-    case '+':
-        for (let i = 1; i < n; i++) {
-            out = out + arr[i]
-        } break;
-    case '-':
-        for (let i = 1; i < n; i++) {
-            out = out - arr[i]
-        } break;
-    case '/':
-        for (let i = 1; i < n; i++) {
-            out = out / arr[i]
-        } break;
-    case '*':
-        for (let i = 1; i < n; i++) {
-            out = out * arr[i]
-        } break;
-    default:
-        throw ('Not a valid operator.')
+
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator!');
+    console.log('==========================');
 }
-console.log('The answer is: ' + out);
+
+function performOneCalculation() {
+    const op = askForInput('Please enter the operator: ');
+    const n = +askForInput('How many numbers would you like to ' + op + '?');
+    let arr = collectNumbers(n);
+    operate(arr, op, n);
+}
+
+function operate(array, operator, amount){
+    var out = array[0];
+    switch (operator) {
+        case '+':
+            for (let i = 1; i < amount; i++) {
+                out = out + array[i]
+            } break;
+        case '-':
+            for (let i = 1; i < amount; i++) {
+                out = out - array[i]
+            } break;
+        case '/':
+            for (let i = 1; i < amount; i++) {
+                out = out / array[i]
+            } break;
+        case '*':
+            for (let i = 1; i < amount; i++) {
+                out = out * array[i]
+            } break;
+        default:
+            throw ('Not a valid operator.')
+        }
+    console.log('The answer is: ' + out);
+}
+
+function collectNumbers(amount){
+    let temp = [];
+    for (let i = 0; i < amount; i++) {
+        temp[i] = +askForInput('Please enter number ' + (i+1) +': ');
+        if (isNaN(temp[i])){
+            console.log('Sorry, please enter a valid number.');
+            i -= 1;
+        }
+    }
+    return temp;
+}
+
+function askForInput(question){
+    console.log(question);
+    const response = readline.prompt();
+    return response;
+}
